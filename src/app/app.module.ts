b12b1af7043a034e9aa809fc3929183ea7d4e985
GMatorin <gleb.matorin@gmail.com>
2021-06-27 15:15:08 -0400

    added header and search layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatTabsModule,
     MatFormFieldModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/dog-api.service.ts
+++ src/app/services/dog-api.service.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/common/http';
 import { partApi2 } from 'src/shared/part-api2';
 import { partApi } from 'src/shared/part-api';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DogApiService {
@@ -16,15 +17,30 @@ export class DogApiService {
     this.klych = partApi2[1] + partApi + partApi2[0];
   }
 
-  getDogInfoByBreed(breed: string): any {
+  getDogInfoByBreed(breed: string): Observable<any> {
     const url = this.url.replace(':breed', breed.toString());
     const header: HttpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
       'x-api-key': this.klych,
     });
 
-    this.http.get(url, { headers: header }).subscribe((info) => {
-      console.log(info);
-    });
+    // this.http.get(url, { headers: header }).subscribe((info) => {
+    //   console.log(JSON.stringify(info));
+    // });
+    return of(response);
   }
 }
+
+const response = [
+  {
+    weight: { imperial: '55 - 120', metric: '25 - 54' },
+    height: { imperial: '26 - 28', metric: '66 - 71' },
+    id: 52,
+    name: 'Borzoi',
+    bred_for: 'Coursing wolves',
+    breed_group: 'Hound',
+    life_span: '10 - 13 years',
+    temperament:
+      'Respectful, Independent, Athletic, Gentle, Intelligent, Quiet',
+  },
+];
